732746026cb090a56c77641084949e9771c547a9
Eduards <137129908+fdc-eduards@users.noreply.github.com>
2025-01-12 09:27:14 +0800

    updated scaffolding

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <p>&copy; 2025, <a href="https://tydus016.github.io/#/" target="_blank">eDeveloper</a></p>
+</footer>
+
+<!-- jquery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+<!-- scripts -->
+<script src="../public/assets/js/script.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+
+// this is the root index file
