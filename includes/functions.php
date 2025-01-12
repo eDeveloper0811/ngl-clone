3acc11faa14a9e72c6d8ef900ed15ecbdcef00fd
Eduards <137129908+fdc-eduards@users.noreply.github.com>
2025-01-12 09:36:00 +0800

    added getBaseUrl function

--- includes/functions.php
+++ includes/functions.php
@@ -1,1 +1,9 @@
 <?php
+function getBaseUrl()
+{
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443 ? "https://" : "http://";
+
+    $host = $_SERVER['HTTP_HOST'];
+
+    return $protocol . $host;
+}
